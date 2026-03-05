7fe3d3182065b299056e7aec709810f987576a57
ltopolsk <lukasz.topolski00@gmail.com>
2026-03-05 16:16:25 +0100

    decision fusion

---
+++ models_config/config_late_rgb.py
@@ -0,0 +1,18 @@
+_base_ = ['./config_early.py']
+
+model = dict(
+    backbone=dict(
+        in_channels=3,
+        init_cfg=None
+    ),
+    data_preprocessor=dict(
+        type='LateFusionDataPreprocessor',
+        keep_channels=[0, 1, 2],
+        custom_mean=[123.675, 116.28, 103.53],
+        custom_std=[58.395, 57.12, 57.375],
+        bgr_to_rgb=False,
+        pad_size_divisor=32
+    )
+)
+
+load_from = 'solov2_r50_fpn_1x_coco_20220512_125858-a357fa23.pth'
