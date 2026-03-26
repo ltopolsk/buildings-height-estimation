8a2ed028926eea313801c2cd9e887674efe60e1e
ltopolsk <lukasz.topolski00@gmail.com>
2026-03-26 14:42:53 +0100

    number of epoches for training increased

--- models_config/config_early.py
+++ models_config/config_early.py
@@ -102,3 +102,17 @@ visualizer = dict(
     vis_backends=vis_backends,
     name='visualizer'
 )
+
+train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=50, val_interval=1)
+
+param_scheduler = [
+    dict(
+        type='LinearLR', start_factor=0.001, by_epoch=False, begin=0, end=500),
+    dict(
+        type='MultiStepLR',
+        begin=0,
+        end=24,
+        by_epoch=True,
+        milestones=[16, 22],
+        gamma=0.1)
+]
